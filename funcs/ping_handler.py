4a3d5bc4b6cf8523f7474038652d53246eaa1f7e
ridhwan-aziz <fwindows345@gmail.com>
2021-12-06 18:34:32 +0700

    ping_handler: Just improving some code(s)

--- funcs/ping_handler.py
+++ funcs/ping_handler.py
@@ -1,9 +1,8 @@
 import time
-PONG = "🏓 **PONG!!!**"
 
 async def main(msg, *another):
-    result = await msg.reply_text(PONG)
-    waktu_awal = msg.date
-    waktu_akhir = time.time()
-    waktu_respon = round(waktu_akhir - waktu_awal, 3)
-    return await result.edit_text(f"{PONG}\n⏱ <code>{waktu_respon}'s</code>")
+    result = await msg.reply("🏓 **PONG!!!**")
+    first_time = msg.date
+    last_time = time.time()
+    response_time = round(first_time - last_time, 3)
+    return await result.edit(f"**{result.text}**\n⏱ <code>{response_time}'s</code>")
