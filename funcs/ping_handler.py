7766282a0b6f9687cf617763f4b46a1df9c0aba0
ridhwan-aziz <fwindows345@gmail.com>
2021-12-12 11:45:49 +0700

    Set first_time to Message.edit_date incase if user edited message to ping command

--- funcs/ping_handler.py
+++ funcs/ping_handler.py
@@ -2,7 +2,7 @@ import time
 
 async def main(msg, *another):
     result = await msg.reply("🏓 **PONG!!!**")
-    first_time = msg.date
+    first_time = msg.edit_date if msg.edit_date else msg.date
     last_time = time.time()
     response_time = round(last_time - first_time, 3)
     return await result.edit(f"**{result.text}**\n⏱ <code>{response_time}'s</code>")
