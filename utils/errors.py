9355f57712c70856cb21071db7d6d5d366231a27
ridhwan-aziz <fwindows345@gmail.com>
2022-02-11 13:54:46 +0700

    Just send error traceback. Don't send link of error

--- utils/errors.py
+++ utils/errors.py
@@ -12,28 +12,7 @@ async def errors_handler(error, event: Message, traceback):
     elif isinstance(error, errors.ChatAdminRequiredError):
         return await event.reply((await lang.get("me_not_admin")))
     else:
-        chat = await event.get_chat()
-        if not event.is_private:
-            if chat.username != None:
-                link = f"https://t.me/{chat.username}/{event.id}"
-            else:
-                if chat.megagroup == True:
-                    link = f"https://t.me/-100{chat.id}/{event.id}"
-                else:
-                    link = f"https://t.me/{chat.id}/{event.id}"
-        else:
-            link = None
-        await event.client.send_message(
-            owner,
-            "Sir, i went wrong while executing this",
-            formatting_entities = [MessageEntityTextUrl(
-                offset=34,
-                length = 4,
-                url = link
-            )] if link else None,
-            link_preview = False
-        )
-        await event.reply(f"Something went wrong\n\n`{str(error)}`")
+        await event.reply(f"Something went wrong\n\n`{str(error)}`\n\nPlease report it to @Pra210906")
         return await event.client.send_message(owner, str(traceback))
 
     
