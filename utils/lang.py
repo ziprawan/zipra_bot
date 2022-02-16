9270b528681e4f68d6a18d5bde5b858dea893dc9
Aziz Ridhwan <fwindows345@gmail.com>
2022-02-16 16:48:44 +0700

    Rebased XML
    utils/langs: Splitted XML into each file for messages and miscellaneous (Like buttons and callback answers)
    utils/lang: Adapted rebased XML
    utils/lang: Remove unused import

--- utils/lang.py
+++ utils/lang.py
@@ -2,12 +2,23 @@ from telethon.tl.custom.message import Message
 from utils.helper import Database
 from utils.init import supported_lang
 import xml.etree.ElementTree as ElementTree
-import os, asyncio
+import os
 
 class Language:
     def __init__(self, msg: Message):
         # Declare variables
         db = Database('groups.db')
+
+        self.db = db
+        self.msg = msg
+    
+    async def get(self, string_name: str, is_misc: bool = False) -> str or None:
+        # Check types
+        if not isinstance(string_name, str) or not isinstance(is_misc, bool):
+            raise TypeError
+
+        db = self.db
+        msg = self.msg
 
         db.exec("""
         CREATE TABLE IF NOT EXISTS lang (
@@ -24,36 +35,28 @@ class Language:
         else:
             lang = fetched[0][0]
             if lang in supported_lang:
-                self.lang_code = lang
+                lang_code = lang
             else:
-                self.lang_code = 'en'
-    
-    async def get(self, string_name: str, is_misc: bool = False) -> str or None:
-        # Check types
-        if not isinstance(string_name, str) or not isinstance(is_misc, bool):
-            raise TypeError
+                lang_code = 'en'
 
-        lang = self.lang_code
+        lang = lang_code
 
         # Read lang file
-        file = f'utils/langs/{lang}/string.xml'
+        if is_misc:
+            file = f'utils/langs/{lang}/misc.xml'
+        else:
+            file = f'utils/langs/{lang}/messages.xml'
+        
         if not os.path.exists(file):
             raise FileNotFoundError("Language file not found!")
-        
-        parsed = ElementTree.parse(file)
-        
-        root = parsed.getroot()
 
-        if is_misc:
-            root = root[1]
-        else:
-            root = root[0]
+        parsed = ElementTree.parse(file)
+        root = parsed.getroot()
         
         lang_data = {}
         for i in root:
             if i.tag == 'string':
                 lang_data[i.attrib['name']] = i.text
-        
         if string_name in lang_data:
             return lang_data[string_name].replace(r"\n", "\n")
         else:
