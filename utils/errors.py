eca9f2f03d2ba21acd3d551096061fd10293cbdf
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2022-03-05 17:53:57 +0700

    Changelog for utils/errors.py:
    - Added MessageTooLongError handling. If the message to be sent is long, it will be converted into a file.
    - Other.

--- utils/errors.py
+++ utils/errors.py
@@ -4,6 +4,8 @@ from telethon.tl.types import MessageEntityTextUrl
 from utils.init import owner
 from telethon import errors
 from utils.lang import Language
+from io import BytesIO
+import time
 
 async def errors_handler(error, event: Message, traceback):
     lang = Language(event)
@@ -11,8 +13,13 @@ async def errors_handler(error, event: Message, traceback):
         return await asyncio.sleep(error.seconds)
     elif isinstance(error, errors.ChatAdminRequiredError):
         return await event.reply((await lang.get("me_not_admin")))
+    elif isinstance(error, errors.MessageTooLongError):
+        msg = error.request.message
+        with BytesIO(str.encode(msg)) as out:
+            out.name = f"{error.__class__.__name__}_{round(time.time())}.txt"
+            return await event.respond(file=out)
     else:
         await event.respond(f"Something went wrong\n\n`{str(error)}`\n\nPlease report it to @Pra210906")
-        return await event.client.send_message(owner, str(traceback), parse_mode=None)
-
-    
+        with BytesIO(str.encode(str(traceback))) as out:
+            out.name = f"{error.__class__.__name__}_{round(time.time())}.txt"
+            return await event.respond(file=out)
