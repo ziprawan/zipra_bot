15d3108f7acb2e160d52998772ff9a033b575c79
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2022-03-15 20:48:22 +0700

    Changed parser algorithm and it's even shorter now :D
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- utils/database.py
+++ utils/database.py
@@ -1,17 +1,21 @@
-import asyncio
+import asyncio, logging
 
 class DatabaseExecutorError(BaseException): ...
 
 class MyDatabase:
     def __init__(self, database):
         self.path = f'databases/{database}'
-    async def exec(self, cmd):
+    async def exec(self, cmd: str):
+        p = cmd.replace('\n', r'\n')
+        logging.debug(f"[Database] Running sqlite command => {p}")
         PIPE = asyncio.subprocess.PIPE
         command = f'sqlite3 {self.path} ".mode list" ".header on" "{cmd}"'
         executed = await asyncio.subprocess.create_subprocess_shell(command, True, PIPE, PIPE)
         stdout, stderr = await executed.communicate()
         if stderr != b'':
+            logging.error(f"[Database] Command error: {stderr.decode()}")
             raise DatabaseExecutorError(f"Error while executing sqlite command.\nError: {stderr.decode()}")
+        logging.debug('[Database] Execute successful. Returning result')
         return stdout.decode()
     async def get_data(self, cmd):
 
@@ -21,6 +25,7 @@ class MyDatabase:
         splitted_by_newline = out.splitlines()
 
         if splitted_by_newline == []:
+            logging.info("[Database] No data found with that query")
             return get_data_result
         
         var_names = splitted_by_newline[0].split("|") 
@@ -30,6 +35,7 @@ class MyDatabase:
             splitted_by_newline.pop(-1) 
 
         if splitted_by_newline == []:
+            logging.info("[Database] No data found with that query")
             return splitted_by_newline
 
         for splitted in splitted_by_newline:
@@ -40,10 +46,12 @@ class MyDatabase:
 
             get_data_result.append(tmp)
 
+        logging.info(f"[Database] {len(get_data_result)} data(s) found with that query")
         return get_data_result
     
     @staticmethod
     def format(text: str):
+        logging.debug("[DatabaseCommandFormatter] Starting format")
         if not isinstance(text, str):
             raise ValueError(f"excpected str, got {type(text)}")
         
@@ -57,6 +65,7 @@ class MyDatabase:
             new_text += tmp
         
         new_text += p
+        logging.debug("[DatabaseCommandFormatter] Returning format")
         return new_text
     
     @staticmethod
