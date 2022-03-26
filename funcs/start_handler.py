8ef6c714d6efb783227dd07009764e520c11250d
Aziz Ridhwan <fwindows345@gmail.com>
2022-03-26 20:37:29 +0700

    funcs/start_handler: Auto format for bot name
    Signed-off-by: Aziz Ridhwan <fwindows345@gmail.com>

--- funcs/start_handler.py
+++ funcs/start_handler.py
@@ -1,13 +1,13 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-START = """Hello, perkenalkan, namaku Zipra.
+START = """Hello, perkenalkan, namaku {}.
 
 Insya Allah, bot ini akan berkembang agar bisa mengatur grup dengan mudah 👍
 
 Kalau pengen lihat bantuan, ketik /help aja 👌"""
 
 async def main(msg: Message, cmd, args):
+    me = await msg._client.get_me()
     if msg.chat.type != "private":
-        me = await msg._client.get_me()
         uname = str(me.username)
         return await msg.reply(
             "O jangan disini 🗿",
@@ -23,4 +23,4 @@ async def main(msg: Message, cmd, args):
     elif args:
         return await msg.reply(args)
     else:
-        await msg.reply_text(START)
+        await msg.reply_text(START.format(me.first_name))
